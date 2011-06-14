09de241b93d42fa1a96549101719fcca3f466d96
Pavel Perestoronin <eigenein@gmail.com>
2011-06-14 18:14:11 +0300

    Commented + default values changed to None

--- protobuf.py
+++ protobuf.py
@@ -19,34 +19,66 @@ class Value:
     '''
     
     def dump(self, fp):
+        '''
+        Dumps the value to the .write()-like object.
+        '''
         raise TypeError('Define in a derived class.')
         
     def load(self, fp):
+        '''
+        Loads the value from the .read()-like object.
+        '''
         raise TypeError('Define in a derived class.')
         
     def pre_validate(self):
+        '''
+        Called before the value will be dumped.
+        '''
         pass
         
     def post_validate(self):
+        '''
+        Called after the value was loaded.
+        '''
         pass
     
 class PrimitiveValue(Value):
+    '''
+    Represents a primitive value.
+    '''
 
     def get_value(self):
+        '''
+        Gets the underlying value.
+        '''
         return self._value
         
     def set_value(self, value):
+        '''
+        Sets the underlying value.
+        '''
         self._value = value
     
     def push_value(self, value):
+        '''
+        Pushes the underlying value. Primitive value cannot be pushed twice.
+        '''
+        if self.is_set():
+            raise ValueError('Primitive value cannot be pushed twice.')
         return self.set_value(value)
         
     def is_set(self):
+        '''
+        Gets whether the value is set.
+        '''
         return not self._value is None
   
 # Varint.
     
 def VarintType():
+    '''
+    Creates a new Varint value instance.
+    '''
     return VarintValue()
 
 class VarintValue(PrimitiveValue):
@@ -54,10 +86,13 @@ class VarintValue(PrimitiveValue):
     Represents a Varint value.
     '''
     
-    def __init__(self, value=0):
+    def __init__(self, value=None):
         self.set_value(value)
     
     def dump_value(self, fp, value):
+        '''
+        Dumps the value to .write()-like object.
+        '''
         if value == 0:
             fp.write('\x00')
         elif not value is None:
@@ -70,6 +105,9 @@ class VarintValue(PrimitiveValue):
         self.dump_value(fp, self.get_value())
     
     def load_value(self, fp):
+        '''
+        Loads the value from .read()-like object and returns it.
+        '''
         value = shift = 0
         while True:
             code = ord(fp.read(1))
@@ -92,11 +130,17 @@ class VarintValue(PrimitiveValue):
 # Signed Varint.
 
 def SignedVarintType():
+    '''
+    Creates a new instance of signed Varint type.
+    '''
     return SignedVarintValue()
 
 class SignedVarintValue(VarintValue):
+    '''
+    Represents a signed Varint value.
+    '''
     
-    def __init__(self, value=0):
+    def __init__(self, value=None):
         self.set_value(value)
     
     def pre_validate(self):
@@ -122,11 +166,17 @@ class SignedVarintValue(VarintValue):
 # Fixed32.
 
 def Fixed32Type():
+    '''
+    Creates a new instance of sfixed32 type.
+    '''
     return Fixed32Value()
 
 class Fixed32Value(PrimitiveValue):
+    '''
+    Represents a signed fixed32 value.
+    '''
     
-    def __init__(self, value='\x00\x00\x00\x00'):
+    def __init__(self, value=None):
         self.set_value(value)
         
     def pre_validate(self):
@@ -147,11 +197,14 @@ class Fixed32Value(PrimitiveValue):
         self.set_value(self.load_value(fp))
 
 def Int32Type():
+    '''
+    Creates an instance of signed int32 type.
+    '''
     return Int32Value()
     
 class Int32Value(Fixed32Value):
     
-    def __init__(self, value=0):
+    def __init__(self, value=None):
         self.set_value(value)
         
     def dump(self, fp):
@@ -167,9 +220,15 @@ class Int32Value(Fixed32Value):
                 raise ValueError('Must be int or long.')
 
 def UInt32Type():
+    '''
+    Creates an instance of unsigned int32 type.
+    '''
     return UInt32Value()
     
 class UInt32Value(Fixed32Value):
+    '''
+    Represents an unsigned int32 value.
+    '''
     
     def __init__(self, value=0):
         self.set_value(value)
@@ -189,11 +248,17 @@ class UInt32Value(Fixed32Value):
                 raise ValueError('Unsigned value must be non-negative.')
 
 def Float32Type():
+    '''
+    Creates an instance of float32 type.
+    '''
     return Float32Value()
 
 class Float32Value(Fixed32Value):
+    '''
+    Represents an float32 value type.
+    '''
     
-    def __init__(self, value=0.0):
+    def __init__(self, value=None):
         self.set_value(value)
         
     def dump(self, fp):
@@ -210,11 +275,17 @@ class Float32Value(Fixed32Value):
 # Fixed64.
 
 def Fixed64Type():
+    '''
+    Creates an instance of fixed64 value.
+    '''
     return Fixed64Value()
 
 class Fixed64Value(PrimitiveValue):
+    '''
+    Represents a fixed64 value.
+    '''
     
-    def __init__(self, value='\x00\x00\x00\x00\x00\x00\x00\x00'):
+    def __init__(self, value=None):
         self.set_value(value)
         
     def pre_validate(self):
@@ -229,11 +300,17 @@ class Fixed64Value(PrimitiveValue):
         self.set_value(fp.read(8))
 
 def Int64Type():
+    '''
+    Creates an instance of signed int64 type.
+    '''
     return Int64Value()
     
 class Int64Value(Fixed64Value):
+    '''
+    Represents a signed int64 value.
+    '''
     
-    def __init__(self, value=0):
+    def __init__(self, value=None):
         self.set_value(value)
         
     def dump(self, fp):
@@ -249,9 +326,15 @@ class Int64Value(Fixed64Value):
                 raise ValueError('Must be int or long.')
 
 def UInt64Type():
+    '''
+    Creates an instance of unsigned int64 type.
+    '''
     return UInt64Value()
     
 class UInt64Value(Fixed64Value):
+    '''
+    Represents an unsigned int64 value.
+    '''
     
     def __init__(self, value=0):
         self.set_value(value)
@@ -271,9 +354,15 @@ class UInt64Value(Fixed64Value):
                 raise ValueError('Unsigned value must be non-negative.')
 
 def Float64Type():
+    '''
+    Creates an instance of float64 type.
+    '''
     return Float64Value()
 
 class Float64Value(Fixed64Value):
+    '''
+    Represents a float64 value.
+    '''
     
     def __init__(self, value=0.0):
         self.set_value(value)
@@ -297,27 +386,49 @@ class MessageType(ValueType):
     '''
     
     def __init__(self, *fields):
+        '''
+        Constructs a new message type with the specified fields. The parameter
+        should be an iterable of tuples (field_name, field_type).
+        '''
         self._fields = fields
         
     def __call__(self):
-        return MessageValue(*self._fields)
+        '''
+        Creates an instance of the message type.
+        '''
+        return MessageInstance(*self._fields)
 
-class MessageValue(Value):
+class MessageInstance(Value):
     '''
-    Represents a message value.
+    Represents a message instance.
     '''
     
     def __init__(self, *fields):
+        '''
+        Initializes a new instance.
+        '''
         self._field_names = tuple(field[0] for field in fields)
         self._field_types = dict(fields)
         self._field_values = dict((field_name, field_type()) for field_name, field_type in fields)
     
     def __getitem__(self, field_name):
+        '''
+        Gets the value of the field.
+        '''
         return self._field_values[field_name].get_value()
         
     def __setitem__(self, field_name, field_value):
+        '''
+        Sets the value of the field.
+        '''
         self._field_values[field_name].set_value(field_value)
         return field_value
+        
+    def __delitem__(self, field_name):
+        '''
+        Deletes the value of the field. Actually, sets it to None.
+        '''
+        self._field_values[field_name].set_value(None)
         
     def dump(self, fp):
         self.pre_validate()
@@ -338,6 +449,12 @@ class MessageValue(Value):
     def post_validate(self):
         for field_name, field_value in self._field_values.iteritems():
             field_value.post_validate()
+            
+    def keys(self):
+        return self._field_names
+        
+    def iterkeys(self):
+        return iter(self._field_names)
 
 _WIRE_TYPES = {
     VarintType: 0,

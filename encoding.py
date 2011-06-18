e48000ad9f202ede16de1662d895fe0ee5f8fa4a
Pavel Perestoronin <eigenein@gmail.com>
2011-06-18 22:36:14 +0300

    Source commented.

--- encoding.py
+++ encoding.py
@@ -7,22 +7,40 @@ import struct
 # Types. -----------------------------------------------------------------------
 
 class Type:
+    '''
+    Represents a general field type.
+    '''
     
     def dump(self, fp, value):
+        '''
+        Dumps its value to write-like object.
+        '''
         raise TypeError('Don\'t call this directly.')
     
     def load(self, fp):
+        '''
+        Loads its value from read-like object and returns a read value.
+        '''
         raise TypeError('Don\'t call this directly.')
     
     def dumps(self, value):
+        '''
+        Dumps its value to string and returns this string.
+        '''
         fp = cStringIO.StringIO()
         self.dump(fp, value)
         return fp.getvalue()
     
     def loads(self, s):
+        '''
+        Loads its value from a string and returns a read value.
+        '''
         return self.load(cStringIO.StringIO(s))
         
 class UVarintType(Type):
+    '''
+    Represents an unsigned Varint type.
+    '''
 
     WIRE_TYPE = 0
 
@@ -41,6 +59,9 @@ class UVarintType(Type):
         return value
         
 class VarintType(UVarintType):
+    '''
+    Represents a signed Varint type. Implements ZigZag encoding.
+    '''
     
     def dump(self, fp, value):
         UVarintType.dump(self, fp, abs(value) * 2 - (1 if value < 0 else 0))
@@ -50,6 +71,9 @@ class VarintType(UVarintType):
         return -div if mod == 0 else div
       
 class BoolType(UVarintType):
+    '''
+    Represents a boolean type. Encodes True as UVarint 1, and False as UVarint 0.
+    '''
 
     def dump(self, fp, value):
         UVarintType.dump(self, fp, int(value))
@@ -58,6 +82,9 @@ class BoolType(UVarintType):
         return bool(UVarintType.load(self, fp))
         
 class StringType(Type):
+    '''
+    Represents a string type.
+    '''
     
     WIRE_TYPE = 2
     
@@ -70,6 +97,10 @@ class StringType(Type):
         return fp.read(length)
 
 class FixedLengthType(Type):
+    '''
+    Represents a general fixed-length value type. You should not use this type
+    directly. Use derived types instead.
+    '''
 
     def dump(self, fp, value):
         fp.write(value)
@@ -78,13 +109,19 @@ class FixedLengthType(Type):
         return fp.read(self.length())
 
 class Fixed64Type(FixedLengthType):
-    
+    '''
+    Represents a general 64-bit value type.
+    '''
+        
     WIRE_TYPE = 1
     
     def length(self):
         return 8
 
 class Fixed32Type(FixedLengthType):
+    '''
+    Represents a general 32-bit value type.
+    '''
 
     WIRE_TYPE = 5
 
@@ -92,6 +129,9 @@ class Fixed32Type(FixedLengthType):
         return 4
 
 class Fixed64SubType(Fixed64Type):
+    '''
+    Represents a general pickle'able 64-bit value type.
+    '''
 
     def dump(self, fp, value):
         Fixed64Type.dump(self, fp, struct.pack(self.format, value))
@@ -100,18 +140,30 @@ class Fixed64SubType(Fixed64Type):
         return struct.unpack(self.format, Fixed64Type.load(self, fp))[0]
         
 class UInt64Type(Fixed64SubType):
+    '''
+    Represents an unsigned int64 type.
+    '''
     
     format = '>Q'
 
 class Int64Type(Fixed64SubType):
+    '''
+    Represents a signed int64 type.
+    '''
     
     format = '>q'
         
 class Float64Type(Fixed64SubType):
+    '''
+    Represents a double precision floating point type.
+    '''
 
     format = 'd'
 
 class Fixed32SubType(Fixed32Type):
+    '''
+    Represents a pickle'able 32-bit value.
+    '''
 
     def dump(self, fp, value):
         Fixed32Type.dump(self, fp, struct.pack(self.format, value))
@@ -120,18 +172,28 @@ class Fixed32SubType(Fixed32Type):
         return struct.unpack(self.format(), Fixed32Type.load(self, fp))[0]
 
 class UInt32Type(Fixed32SubType):
+    '''
+    Represents an unsigned int32 type.
+    '''
     
     format = '>I'
 
 class Int32Type(Fixed32SubType):
+    '''
+    Represents a signed int32 type.
+    '''
     
     format = '>i'
         
 class Float32Type(Fixed32SubType):
+    '''
+    Represents a single precision floating point type.
+    '''
 
     format = 'f'
 
 # Types instances. -------------------------------------------------------------
+# You should actually use these types instances when defining your message type.
 
 UVarint = UVarintType()
 Varint = VarintType()
@@ -153,9 +215,9 @@ class Flags:
     Flags for a field.
     '''
 
-    SIMPLE = 0
-    REQUIRED, REQUIRED_MASK = 1, 1
-    SINGLE, REPEATED, PACKED_REPEATED, REPEATED_MASK = 0, 2, 6, 6
+    SIMPLE = 0 # Single value field.
+    REQUIRED, REQUIRED_MASK = 1, 1 # Required field_type.
+    SINGLE, REPEATED, PACKED_REPEATED, REPEATED_MASK = 0, 2, 6, 6 # Repeated and packed-repeated fields.
 
 class _EofWrapper:
     '''
@@ -165,17 +227,27 @@ class _EofWrapper:
         self.__fp = fp
         
     def read(self, size=None):
+        '''
+        Reads a string. Raises EOFError on end of stream.
+        '''
         s = self.__fp.read(size)
         if len(s) == 0:
-            raise EOFError()
+            raise EOFError('The underlying stream has read the empty string.')
         return s
 
 def _pack_key(tag, wire_type):
+    '''
+    Packs a tag and a wire_type into single int according to the protobuf spec.
+    '''
     return (tag << 3) | wire_type
     
 def _unpack_key(key):
+    '''
+    Unpacks a key into a tag and a wire_type according to the protobuf spec.
+    '''
     return key >> 3, key & 7
 
+# This used to correctly determine the length of unknown tags when loading a message.
 _wire_type_to_type_instance = {
     0: Varint,
     1: Fixed64,
@@ -184,14 +256,23 @@ _wire_type_to_type_instance = {
 }
 
 class MessageType(Type):
+    '''
+    Represents a message type.
+    '''
 
     def __init__(self):
-        self.__tags_to_types = dict()
-        self.__tags_to_names = dict()
-        self.__flags = dict()
-        self.__defaults = dict()
+        '''
+        Creates a new message type.
+        '''
+        self.__tags_to_types = dict() # Maps a tag to a type instance.
+        self.__tags_to_names = dict() # Maps a tag to a given field name.
+        self.__flags = dict() # Maps a tag to flags.
+        self.__defaults = dict() # Maps a tag to a default value of the field.
 
     def add_field(self, tag, name, field_type, default=None, flags=Flags.SIMPLE):
+        '''
+        Adds a field to the message type.
+        '''
         if tag in self.__tags_to_names or tag in self.__tags_to_types:
             raise ValueError('The tag %s is already used.' % tag)
         self.__tags_to_names[tag] = name
@@ -201,6 +282,9 @@ class MessageType(Type):
             self.__defaults[tag] = default
 
     def remove_field(self, tag):
+        '''
+        Removes a field by its tag.
+        '''
         if tag in self.__tags_to_names:
             del self.__tags_to_names[tag]
         if tag in self.__tags_to_types:
@@ -209,16 +293,22 @@ class MessageType(Type):
             del self.__defaults[tag]
 
     def __call__(self):
+        '''
+        Creates an instance of this message type.
+        '''
         return Message(self, self.__defaults)
 
     def __has_flag(self, tag, flag, mask):
+        '''
+        Checks whether the field with the specified tag has the specified flag.
+        '''
         return (self.__flags[tag] & mask) == flag
 
     def dump(self, fp, value):
         for tag, field_type in self.__tags_to_types.iteritems():
             if self.__tags_to_names[tag] in value:
                 if self.__has_flag(tag, Flags.SINGLE, Flags.REPEATED_MASK):
-                    # Simple value.
+                    # Single value.
                     UVarint.dump(fp, _pack_key(tag, field_type.WIRE_TYPE))
                     field_type.dump(fp, value[self.__tags_to_names[tag]])
                 elif self.__has_flag(tag, Flags.PACKED_REPEATED, Flags.REPEATED_MASK):
@@ -253,6 +343,7 @@ class MessageType(Type):
                     elif wire_type != String.WIRE_TYPE:
                         raise TypeError('Tag %s has wiretype %s while the field is packed repeated.' % tag)
                     if self.__has_flag(tag, Flags.SINGLE, Flags.REPEATED_MASK):
+                        # Single value.
                         message[self.__tags_to_names[tag]] = field_type.load(fp)
                     elif self.__has_flag(tag, Flags.PACKED_REPEATED, Flags.REPEATED_MASK):
                         # Repeated packed value.
@@ -275,40 +366,73 @@ class MessageType(Type):
                 return message
 
 class Message(dict):
+    '''
+    Represents a message instance.
+    '''
 
     def __init__(self, message_type, defaults=dict()):
+        '''
+        Initializes a new instance of the specified message type.
+        '''
         self.message_type = message_type
         self.update(defaults)
         
     def __getattr__(self, name):
+        '''
+        Gets a value of the specified message field.
+        '''
         return self.__getitem__(name)
         
     def __setattr__(self, name, value):
+        '''
+        Sets a value of the specified message field.
+        '''
         (self.__dict__ if name in self.__dict__ else self).__setitem__(name, value)
         return value
     
     def __delattr__(self, name):
+        '''
+        Removes a value of the specified message field.
+        '''
         (self.__dict__ if name in self.__dict__ else self).__delitem__(name, value)
         
     def dumps(self):
+        '''
+        Dumps the message into a string.
+        '''
         return self.message_type.dumps(self)
     
     def dump(self, fp):
+        '''
+        Dumps the message into a write-like object.
+        '''
         return self.message_type.dump(self, value)   
 
 def loads(self, s, message_type):
+    '''
+    Loads a message of the specified message type from the string.
+    '''
     return message_type.loads(s)
     
 def load(self, fp, message_type):
+    '''
+    Loads a message of the specified message type from the read-like object.
+    '''
     return message_type.load(fp)
 
 # Embedded message. ------------------------------------------------------------
 
 class EmbeddedMessage(Type):
+    '''
+    Represents an embedded message type.
+    '''
     
     WIRE_TYPE = 2
     
     def __init__(self, message_type):
+        '''
+        Initializes a new instance. The argument is an underlying message type.
+        '''
         self.message_type = message_type
     
     def dump(self, fp, value):

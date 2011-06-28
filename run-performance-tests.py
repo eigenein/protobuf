a54721621ab8210f9b6735a2d17b504a25d93e82
Pavel Perestoronin <eigenein@gmail.com>
2011-06-29 00:35:03 +0300

    perf tests

--- run-performance-tests.py
+++ run-performance-tests.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import timeit
+from protobuf import *
 
 tests = list()
 test_cases = dict()
+number = 100000
 
 def test(title):
     def wrapper(target):
@@ -15,7 +17,7 @@ def test(title):
 def testcase(*args, **kwargs):
     def wrapper(target):
         if target.__name__ in test_cases:
-            test_cases[target.__name__].append((args, kwargs))
+            test_cases[target.__name__].insert(0, (args, kwargs))
         else:
             test_cases[target.__name__] = [(args, kwargs)]
         return target
@@ -23,30 +25,47 @@ def testcase(*args, **kwargs):
         
 def runtests():
     for title, target in tests:
-        if not target in test_cases:
-            stmt = '%s()' % target
-            print title
-            print '%s: %ss' % (stmt, min(timeit.repeat(stmt, 'import protobuf; from __main__ import %s' % target)))
-        else:
-            for args, kwargs in test_cases[target]:
-                args_str = ', '.join(str(arg) for arg in args)
-                kwargs_str = ', '.join('%s=%s' % (k, v) for k, v in kwargs.iteritems())
-                allargs_str = args_str
-                allargs_str += ', ' + kwargs_str if len(args_str) > 0 and len(kwargs_str) > 0 else kwargs_str
-                stmt = '%s(%s)' % (target, allargs_str)
-                print title
-                print '%s: %ss' % (stmt, min(timeit.repeat(stmt, 'import protobuf; from __main__ import %s' % target)))
-        print '---------------------------------------------------------------'
+        for args, kwargs in test_cases[target]:
+            args_str = ', '.join(repr(arg) for arg in args)
+            kwargs_str = ', '.join('%s=%s' % (k, repr(v)) for k, v in kwargs.iteritems())
+            allargs_str = args_str
+            allargs_str += ', ' + kwargs_str if len(args_str) > 0 and len(kwargs_str) > 0 else kwargs_str
+            stmt = '%s(%s)' % (target, allargs_str)
+            print '\033[95mTest: %s\033[0m' % title
+            print '%s: \033[92m%.4fs\033[0m' % (stmt.ljust(70), min(timeit.repeat(stmt, 'from __main__ import %s' % target, number=number)))
+        print '\033[94m%s\033[0m' % '-' * 80
         
+# Fake output. -----------------------------------------------------------------
+
+class FakeOutput:
+    
+    def write(self, s):
+        pass
+
+fp = FakeOutput()      
+
 # Tests themselves. ------------------------------------------------------------
 
-@test('testtest')
-@testcase()
-def testtest():
-    pass
+@test('UVarint dump')
+@testcase(0)
+@testcase(1)
+@testcase(0x7FFFFFFF)
+@testcase(12345678901234567890L)
+def test_uvarint_dump(value):
+    UVarint.dump(fp, value)
 
 # Main. ------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    runtests()
+    print
+    try:
+        runtests()
+    except KeyboardInterrupt:
+        print
+        print '\033[91mInterrupted.\033[0m'
+        print
+    else:
+        print 
+        print '\033[92mDone.\033[0m'
+        print
 

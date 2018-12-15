2409e861cb699ff5522ec64c8936c0171f300ad5
Pavel Perestoronin <pavel.perestoronin@kpn.com>
2018-12-15 14:19:55 +0100

    Add long description and classifiers, add JetBrains to gitignore

--- setup.py
+++ setup.py
@@ -6,9 +6,18 @@ setup(
     description=(
         'Python implementation of Protocol Buffer (protobuf) data types'
     ),
-    author='eigenein',
+    long_description=open('README.markdown').read(),
+    long_description_content_type='text/markdown',
+    author='Pavel Perestoronin',
     author_email='eigenein@gmail.com',
     url='https://github.com/eigenein/protobuf',
     packages=find_packages(exclude=['tests']),
     install_requires=[],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )

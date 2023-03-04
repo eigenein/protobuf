bc435db615be61bd46d0fbf7bfad88788fb48a63
Pavel Perestoronin <eigenein@gmail.com>
2023-03-04 15:20:27 +0100

    Fix: `@dataclass` parameters for 3.8 & 3.9

--- pure_protobuf/descriptors/record.py
+++ pure_protobuf/descriptors/record.py
@@ -11,7 +11,7 @@ from pure_protobuf._accumulators import AccumulateLastOneWins
 from pure_protobuf._mergers import MergeLastOneWins
 from pure_protobuf.annotations import double, fixed32, fixed64, int32, int64, sfixed32, sfixed64, uint
 from pure_protobuf.exceptions import UnsupportedAnnotationError
-from pure_protobuf.helpers._dataclasses import SLOTS
+from pure_protobuf.helpers._dataclasses import SLOTS, KW_ONLY
 from pure_protobuf.helpers.itertools import ReadCallback
 from pure_protobuf.interfaces._vars import RecordT
 from pure_protobuf.interfaces.accumulate import Accumulate
@@ -40,7 +40,7 @@ if TYPE_CHECKING:
     from pure_protobuf.message import BaseMessage
 
 
-@dataclass(kw_only=True, **SLOTS)
+@dataclass(**KW_ONLY, **SLOTS)
 class RecordDescriptor(Generic[RecordT]):
     """
     Describes how records should be read, written, accumulated, and merged.
@@ -91,7 +91,7 @@ class RecordDescriptor(Generic[RecordT]):
         Examples:
             - For `Annotated[int, Field[1]]` the inner hint is `int`.
             - For `Annotated[List[int], Field[1]]` it's also `int`
-              since the `List` has already been extracted by `from_annotated`.
+              since the `List` has already been extracted by `Field._from_annotated_args`.
 
         Args:
             message_type: message type which contains the attribute being described
